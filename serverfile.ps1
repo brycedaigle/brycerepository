7cd747e18c14602bb6f4ffbe3e4ae0b77e45cdae
brycedaigle <myemail>
2023-10-25 15:03:35 -0400

    create objext and things

--- serverfile.ps1
+++ serverfile.ps1
@@ -2,3 +2,12 @@
 $Info = Get-ComputerInfo | select-object CsDNSHostName, OsVersion
 $Info
 # get operating system version
+
+# get disk info
+$DiskInfo = Get-Disk
+$DiskInfo.count 
+
+$obj = [PSCustomObject]@{
+    LogicalDiskCount = $DiskInfo.count
+}
+Write-Output $obj
