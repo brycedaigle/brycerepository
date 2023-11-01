3ff5bbf0839e4260284d1824108dfe1e6fec8ebc
brycedaigle <myemail>
2023-11-01 14:12:28 -0400

    Changes made

--- serverfile.ps1
+++ serverfile.ps1
@@ -1,15 +1,16 @@
 # Gets the computer name ;)
-$Info = Get-ComputerInfo | select-object CsDNSHostName, OsVersion
-$Info
+$ComputerInfo = Get-ComputerInfo 
 # get operating system version
 
 # get disk info
 $DiskInfo = Get-Disk
-$DiskInfo.count 
+
+$FreeSpace = Get-PSDrive | WhereObject {$_.Name -eq 'C'}
 
 $obj = [PSCustomObject]@{
     ComputerName = $ComputerInfo.CsDNSHostName
     OsVersion = $ComputerInfo.OsVersion
     LogicalDiskCount = $DiskInfo.count
+    FreeSpace = $FreeSpace.Free 
 }
 Write-Output $obj
